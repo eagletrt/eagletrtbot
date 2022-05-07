825491dc98fc5eb43cab8d0565a6cfd2a09c1522
Filippo Rossi <filipporossi@hey.com>
2022-05-07 14:48:50 +0200

    feat: fix instagram duplicate notification

--- bot/tasks/instagram.py
+++ bot/tasks/instagram.py
@@ -28,9 +28,9 @@ def send_media(media: Media):
     url = f"https://www.instagram.com/p/{media.code}"
     text = f"{caption}\n\n🌐 {url}"
     if media.media_type == PHOTO:
-        message = BOT.send_photo(config.SPAM, media.thumbnail_url, caption=text)
+        BOT.send_photo(config.SPAM, media.thumbnail_url, caption=text)
     elif media.media_type == VIDEO:
-        message = BOT.send_video(config.SPAM, media.video_url, caption=text)
+        BOT.send_video(config.SPAM, media.video_url, caption=text)
     elif media.media_type == ALBUM:
         if len(media.resources) > 10:
             media.resources = media.resources[:10]
@@ -41,10 +41,9 @@ def send_media(media: Media):
             for resource in media.resources
         ]
         resources[0].caption = text
-        message = BOT.send_media_group(config.SPAM, resources)
+        BOT.send_media_group(config.SPAM, resources)
     with open(LIKE_AND_SAVE, "rb") as sticker:
         BOT.send_sticker(config.SPAM, sticker)
-    message.pin(False)
 
 
 def task():
@@ -77,7 +76,7 @@ def task():
                 session.query(InstagramMedia).filter_by(instagram_id=media.id).count()
                 == 0
             ):
+                session.add(InstagramMedia(instagram_id=media.id))
+                session.commit()
                 if not empty:
                     send_media(media)
-                session.add(InstagramMedia(instagram_id=media.id))
-        session.commit()
