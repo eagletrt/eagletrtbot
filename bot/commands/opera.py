00042668b85410f54d33f87036603f3ad29c0047
tzarjakob <giacomo.mazzucchi@protonmail.com>
2024-02-05 20:34:45 +0100

    feat: support for fastenal command

--- bot/commands/opera.py
+++ bot/commands/opera.py
@@ -5,9 +5,8 @@ import os
 
 UNSPLASH_ACCESS_KEY=os.environ['UNSPLASH_ACCESS_KEY']
 
-
-def send_random_monkey_image(update: Update, ctx: CallbackContext):
-    api_url = f'https://api.unsplash.com/photos/random?query=monkey&client_id={UNSPLASH_ACCESS_KEY}'
+def send_photo(update: Update, query: str, default_path: str):
+    api_url = f'https://api.unsplash.com/photos/random?query={query}&client_id={UNSPLASH_ACCESS_KEY}'
     try:
         response = requests.get(api_url)
         response.raise_for_status()
@@ -16,8 +15,17 @@ def send_random_monkey_image(update: Update, ctx: CallbackContext):
         img_data = requests.get(image_url).content
         update.message.bot.send_photo(update.message.chat.id, img_data, reply_to_message_id=update.message.message_id)
     except requests.exceptions.RequestException as e:
-        update.message.bot.send_photo(update.message.chat.id,open("bot/media/default_monkey.png","rb"), reply_to_message_id=update.message.message_id)
+        update.message.bot.send_photo(update.message.chat.id,open(default_path,"rb"), reply_to_message_id=update.message.message_id)  
+
+
+def send_random_monkey_image(update: Update, ctx: CallbackContext):
+    send_photo(update, "monkey", "bot/media/default_monkey.png")
+
+
+def send_random_transparent_stuff_image(update: Update, ctx: CallbackContext):
+    send_photo(update, "transparent", "bot/media/default_transparent.jpg")
 
 
 def register(dispatcher: Dispatcher[CallbackContext, dict, dict, dict]):
     dispatcher.add_handler(CommandHandler("opera", send_random_monkey_image))
+    dispatcher.add_handler(CommandHandler("fastenal", send_random_transparent_stuff_image))
